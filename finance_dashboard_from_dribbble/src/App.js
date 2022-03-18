62d51c6db425cf82cc8667a4ed793fd1316958e7
Miller <farwes1180@gmail.com>
2022-03-19 00:40:41 +0900

    navbar

--- finance_dashboard_from_dribbble/src/App.js
+++ finance_dashboard_from_dribbble/src/App.js
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/Home';
+
 function App() {
-  return <div className='App'>APP</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/'>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ finance_dashboard_from_dribbble/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,74 @@
+import './sidebar.scss';
+
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import AccountBalanceOutlinedIcon from '@mui/icons-material/AccountBalanceOutlined';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='top'>
+        <div className='first-row'>
+          <div className='dot' />
+          <div className='dot' />
+        </div>
+        <div className='second-row'>
+          <div className='dot' />
+          <div className='dot' />
+        </div>
+      </div>
+
+      <div className='bottom'>
+        <ul>
+          <li>
+            <HomeRoundedIcon className='icon' />
+            <span className='title'>Home</span>
+          </li>
+
+          <li>
+            <EmojiEventsOutlinedIcon className='icon' />
+            <span className='title'>Projects</span>
+          </li>
+
+          <li>
+            <AccountBalanceOutlinedIcon className='icon' />
+            <span className='title'>Billings</span>
+          </li>
+
+          <li>
+            <PermIdentityOutlinedIcon className='icon' />
+            <span className='title'>Team Members</span>
+          </li>
+
+          <li>
+            <BarChartOutlinedIcon className='icon' />
+            <span className='title'>Stats</span>
+          </li>
+
+          <li>
+            <SettingsOutlinedIcon className='icon' />
+            <p className='title'>Settings</p>
+          </li>
+        </ul>
+      </div>
+
+      <div className='space'></div>
+
+      <div className='profile'>
+        <div className='avatarBox'>
+          <img
+            src='https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+            alt=''
+            className='avatar'
+          />
+        </div>
+        <p className='name'>Joe Pass</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
